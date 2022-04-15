52f768003b529faa9fc1b5df7e692a9b28bb41f8
jsafe00 <josafebalili@gmail.com>
2022-04-15 12:26:46 +0800

    add git

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Container from 'components/Container';
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -9,6 +10,15 @@ const Header = () => {
         <h1>
           Visita Iglesia 2022
         </h1>
+        <p>
+          Best Viewed on Desktop
+        </p>
+        <p className="header-subtitle">
+          <a href="https://github.com/jsafe00/visita-iglesia" target="blank"> 
+            <FaGithub />
+            View on Github
+          </a>
+        </p>
       </Container>
     </header>
   );
