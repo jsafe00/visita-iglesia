07969f40a872359ff08402380d5c2c46eda95f12
jsafe00 <josafebalili@gmail.com>
2022-04-15 18:04:18 +0800

    add g-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingIds: [
+            "G-60J37YERL9",
+          ],
+        pluginConfig: {
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        },
+      },
+    },
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
